c07cd7ae178ce1b37d903c6ba3646b3e5137219c
mason001 <mason001@hancock.pro>
2020-06-18 15:56:05 +0800

    [ADD] fastForwardSpeed to playerSetting

--- MSSPlayer/Classes/PlayerSetting.swift
+++ MSSPlayer/Classes/PlayerSetting.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 public protocol PlayerSetting {
+    var fastForwardSpeed: Double { get set }
     var voiceAdjustSpeed: Double { get set }
     var brightnessAdjustSpeed: Double { get set }
 }
 
 open class MSSPlayerSetting: PlayerSetting {
+    public var fastForwardSpeed: Double = 1.0
     public var voiceAdjustSpeed: Double = 1.0
     public var brightnessAdjustSpeed: Double = 1.0
 }
